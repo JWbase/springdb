ffcfdcd440697bbd40188777ed29aef84769f19f
jwBase <jungwoo1002@gmail.com>
2023-04-04 10:19:50 +0900

    - ConnectionPool, DataSource

--- jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ jdbc/src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -12,6 +12,10 @@ import java.sql.SQLException;
 
 import static hello.jdbc.connection.ConnectionConst.*;
 
+/**
+ * DriverManager 는 커넥션을 획득할 때 마다 URL, USERNAME, PASSWORD 같은 파라미터를 계속 전달해야함
+ * DataSource를 사용하는 방식은 처음 객체를 생성할 때만 필요한 파라미터를 넘겨주고, 커넥션을 획득할 때는 단순히 dataSource.getConnection()만 호출하면됨
+ */
 @Slf4j
 public class ConnectionTest {
 
@@ -19,8 +23,8 @@ public class ConnectionTest {
     void driverManager() throws SQLException {
         Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        log.info("connection={}, class={]", con1, con1, getClass());
-        log.info("connection={}, class={]", con2, con2, getClass());
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
     }
 
     @Test
@@ -41,11 +45,13 @@ public class ConnectionTest {
         dataSource.setPoolName("MyPool");
 
         useDataSource(dataSource);
-        Thread.sleep(1000);//별도 쓰레드에서 추가하기 때문에 sleep걸어주고 로그확인
+        Thread.sleep(1000);// 커넥션 풀에서 커넥션 생성 시간 대기
     }
 
     private void useDataSource(DataSource dataSource) throws SQLException {
         Connection con1 = dataSource.getConnection();
         Connection con2 = dataSource.getConnection();
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
     }
 }
