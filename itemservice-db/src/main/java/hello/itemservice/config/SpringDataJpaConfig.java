9f4e6e282a1293cee497681749cf12311d00eaf0
JWbase <jungwoo1002@gmail.com>
2023-06-13 00:07:35 +0900

    - springDataJpaConfig 설정

--- itemservice-db/src/main/java/hello/itemservice/config/SpringDataJpaConfig.java
+++ itemservice-db/src/main/java/hello/itemservice/config/SpringDataJpaConfig.java
@@ -1,24 +1,21 @@
 package hello.itemservice.config;
 
 import hello.itemservice.repository.ItemRepository;
-import hello.itemservice.repository.jpa.JpaItemRepository;
-import hello.itemservice.repository.mybatis.MyBatisItemRepository;
+import hello.itemservice.repository.jpa.JpaItemRepositoryV2;
+import hello.itemservice.repository.jpa.SpringDataJpaItemRepository;
 import hello.itemservice.service.ItemService;
 import hello.itemservice.service.ItemServiceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
 
+@RequiredArgsConstructor
 @Configuration
-public class JpaConfig {
+public class SpringDataJpaConfig {
 
-    private final EntityManager em;
-
-    public JpaConfig(EntityManager em) {
-        this.em = em;
-    }
+    private final SpringDataJpaItemRepository springDataJpaItemRepository;
 
     @Bean
     public ItemService itemService() {
@@ -27,7 +24,7 @@ public class JpaConfig {
 
     @Bean
     public ItemRepository itemRepository() {
-        return new JpaItemRepository(em);
+        return new JpaItemRepositoryV2(springDataJpaItemRepository);
     }
 
 }
